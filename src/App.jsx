5d2646d428c564dd9c01cff7f331c9f7bfa78380
Mrbabet <wieliczkokamil@gmail.com>
2024-01-21 21:51:54 +0100

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,23 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Movies from "./components/Movies/Movies";
 import Reviews from "./components/Reviews/Reviews";
+import NotFound from "./components/NotFound/NotFound";
+import Cast from "./components/Cast/Cast";
+import MoviesDetails from "./components/MoviesDetails/MoviesDetails";
+
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/goit-react-hw-05-movies/" element={<Home />} />
+        <Route path="/goit-react-hw-05-movies/movies" element={<Movies />} />
+        <Route
+          path="/goit-react-hw-05-movies/moviesdetails"
+          element={<MoviesDetails />}
+        />
+        <Route path="/goit-react-hw-05-movies/reviews" element={<Reviews />} />
+        <Route path="/goit-react-hw-05-movies/cast" element={<Cast />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
