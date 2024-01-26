66ab77e4a7e37597b122ac8bf3d212594b66302c
Mrbabet <wieliczkokamil@gmail.com>
2024-01-26 17:33:50 +0100

    Updates

---
+++ assets/Home-MhSm2HQL.js
@@ -0,0 +1,1 @@
+import{r as e,j as t}from"./index-etG03V3P.js";import{f as a}from"./Api-aYbiQ__Y.js";import{T as o,M as n,a as c}from"./Title-NigLedNC.js";const j=()=>{const[s,i]=e.useState([]);return e.useEffect(()=>{(async()=>{const r=await a();i(r)})()},[]),t.jsxs(t.Fragment,{children:[t.jsx(o,{title:"Trending Movies"}),t.jsx(n,{children:t.jsx(c,{item:s})})]})};export{j as default};
