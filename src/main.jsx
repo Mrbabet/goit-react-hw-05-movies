74f1aad7faea309c7dc13a3df9d60f7a3d17884a
Mrbabet <wieliczkokamil@gmail.com>
2024-01-26 18:18:19 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import "./main.css";
 
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies">
+    <HashRouter basename="/goit-react-hw-05-movies">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
